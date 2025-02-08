59e9451f5a3afc61e00acb199c3d26bacec943dc
sebehhhh <muhammad.seman030801@gmail.com>
2025-02-08 21:52:35 +0800

    penyesuaian dashboard

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,12 +5,22 @@ namespace App\Controllers;
 class Home extends BaseController
 {
 
-    public function index(): mixed // Ubah tipe kembalian menjadi mixed
+    public function index(): mixed
     {
         if (!session()->get('isLoggedIn')) {
-            // Jika tidak, redirect ke halaman login
-            return redirect()->to('/auth/login'); // Ganti '/auth/login' dengan URL login Anda
+            return redirect()->to('/auth/login');
         }
-        return view('content/home');
+    
+        // Mengambil jumlah sarana & kursi yang tersedia
+        $saranaModel = new \App\Models\SaranaModel();
+        $kursiModel = new \App\Models\KursiModel();
+    
+        $data = [
+            'total_sarana' => $saranaModel->countAll(), // Menghitung total sarana
+            'total_kursi' => $kursiModel->where('status_kursi', 'kosong')->countAllResults() // Menghitung kursi tersedia
+        ];
+    
+        return view('content/home', $data);
     }
+    
 }
